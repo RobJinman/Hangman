cac97eede2a82110e3d8c1bbe7056f05df3575f2
Rob Jinman <admin@robjinman.com>
2017-05-01 13:36:05 +0100

    Vagrant files for xenial64

---
+++ vagrant/xenial64/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y g++ \
+  cmake \
+  git \
+  qtbase5-dev \
+  qt5-default
--- vagrant_linux/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get install -y cmake
-apt-get install -y g++
-apt-get install -y git
-apt-get install -y gdb
-apt-get install -y qtdeclarative5-dev
-
